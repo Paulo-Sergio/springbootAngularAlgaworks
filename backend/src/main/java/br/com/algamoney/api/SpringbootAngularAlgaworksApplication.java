a57a90dbecae8454246010195f6348bfda32b95f
Paulo França <paulofranca.contato@gmail.com>
2018-09-04 13:15:06 -0300

    -Iniciando o projeto da API -Conectando ao mysql -Migração de dados com flyway -Criando o recurso de Categoria -Consultando Categorias -Cadastrando nova Categoria -Validando atributos desconhecidos -Tratando erros com exceptionhandler -Validando valores invalidos com bean-validation

---
+++ backend/src/main/java/br/com/algamoney/api/SpringbootAngularAlgaworksApplication.java
@@ -0,0 +1,12 @@
+package br.com.algamoney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngularAlgaworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAngularAlgaworksApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/br/com/paulofranca/algamoney/api/SpringbootAngularAlgaworksApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.paulofranca.algamoney.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootAngularAlgaworksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
