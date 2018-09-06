f2d7c3269a87b9be42a4179499982db3f2718333
Paulo França <paulofranca.contato@gmail.com>
2018-09-05 23:10:21 -0300

    - criando migração e entidade de lançamento - busca de lançamentos - cadastrando primeiro lançamento

---
+++ backend/src/main/java/br/com/algamoney/api/model/TipoLancamento.java
@@ -0,0 +1,7 @@
+package br.com.algamoney.api.model;
+
+public enum TipoLancamento {
+
+	RECEITA,
+	DESPESA
+}
---
+++ backend/src/main/java/br/com/algamoney/api/resource/LancamentoResource.java
@@ -0,0 +1,58 @@
+package br.com.algamoney.api.resource;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.algamoney.api.event.RecursoCriadoEvent;
+import br.com.algamoney.api.model.Lancamento;
+import br.com.algamoney.api.repository.LancamentoRepository;
+
+@RestController
+@RequestMapping("/lancamentos")
+public class LancamentoResource {
+
+	@Autowired
+	private LancamentoRepository lancamentoRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+
+	@GetMapping
+	public List<Lancamento> listar() {
+		List<Lancamento> lancamentos = this.lancamentoRepository.findAll();
+		return lancamentos;
+	}
+
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Lancamento> buscarPeloCodigo(@PathVariable Long codigo) {
+		Lancamento lancamento = this.lancamentoRepository.findOne(codigo);
+		if (lancamento != null) {
+			return ResponseEntity.status(HttpStatus.OK).body(lancamento);
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+	}
+	
+	@PostMapping
+	public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+		Lancamento lancamentoSalvo = this.lancamentoRepository.save(lancamento);
+		
+		this.publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
+	}
+
+}
