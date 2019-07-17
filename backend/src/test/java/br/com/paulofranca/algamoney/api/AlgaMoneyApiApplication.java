75fd74644c6c4806dacad9b26b4e6c186eb1fe75
Paulo-Sergio <paulofranca.contato@gmail.com>
2019-07-17 00:04:25 -0300

    - criando migração e entidade de lançamento - busca de lançamentos - cadastrando primeiro lançamento

--- backend/src/test/java/br/com/paulofranca/algamoney/api/AlgaMoneyApiApplication.java
+++ backend/src/test/java/br/com/paulofranca/algamoney/api/AlgaMoneyApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootAngularAlgaworksApplicationTests {
+public class AlgaMoneyApiApplication {
 
 	@Test
 	public void contextLoads() {
